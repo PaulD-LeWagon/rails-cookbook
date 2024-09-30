0d7aa8a6b045df7eccf2c0059d6b4e2b2765a251
Paul Devanney <pauldevanney92@gmail.com>
2024-09-30 10:02:22 +0100

    Add validation to models refactor

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
-  has_many :recipes, through: :bookmarks, dependent: :destroy
+  has_many :recipes, through: :bookmarks # , dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,6 @@ class Recipe < ApplicationRecord
   has_many :bookmarks
   has_many :categories
 
-  validates :overview, presence: true
-  validates :title, presence: true, uniqueness: true
+  validates :name, :description, :image_url, :rating, presence: true
+  validates :name, uniqueness: true
 end
