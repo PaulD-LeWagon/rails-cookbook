6bc15bf8fa32413e52847a5180dd5514ba1cf39a
Paul Devanney <pauldevanney92@gmail.com>
2024-09-30 09:58:10 +0100

    Add validation to models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,7 @@
 class Recipe < ApplicationRecord
+  has_many :bookmarks
+  has_many :categories
+  # Testing 1, 2, 3...
+  validates :overview, presence: true
+  validates :title, presence: true, uniqueness: true
 end
